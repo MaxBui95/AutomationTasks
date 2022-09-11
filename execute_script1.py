39302d46fa1284fdd559d0934a5f29c9751e9d0b
Maxim Bui <maxim_bui@epam.com>
2022-09-11 16:16:57 +0300

    cleaning the repository

--- execute_script1.py
+++
@@ -1,8 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
-browser = webdriver.Chrome(executable_path="C:\\chromedriver\\chromedriver.exe")
-link = "https://SunInJuly.github.io/execute_script.html"
-browser.get(link)
-button = browser.find_element(By.TAG_NAME, "button")
-button.click()
